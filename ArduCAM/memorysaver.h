e21ade014272d7f7dc9a010504c028a10abc4f17
Mathieu Graillot <55492065+MGraillot@users.noreply.github.com>
2023-01-25 12:03:19 +0100

    Update memorysaver.h

--- ArduCAM/memorysaver.h
+++ ArduCAM/memorysaver.h
@@ -8,12 +8,12 @@
 //Only ArduCAM Shield series platform need to select camera module, ArduCAM-Mini series platform doesn't
 
 //Step 1: select the hardware platform, only one at a time
-//#define OV2640_MINI_2MP
+#define OV2640_MINI_2MP
 //#define OV3640_MINI_3MP
 //#define OV5642_MINI_5MP
 //#define OV5642_MINI_5MP_BIT_ROTATION_FIXED
 //#define OV2640_MINI_2MP_PLUS
-#define OV5642_MINI_5MP_PLUS
+//#define OV5642_MINI_5MP_PLUS
 //#define OV5640_MINI_5MP_PLUS
 
 
@@ -27,7 +27,7 @@
 	//#define OV7725_CAM
 	//#define OV7670_CAM
 	//#define OV7675_CAM
-  //#define OV2640_CAM
+	#define OV2640_CAM
 	//#define OV3640_CAM
 	//#define OV5642_CAM
 	//#define OV5640_CAM 
